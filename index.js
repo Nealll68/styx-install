fb1cc6bdd61f94ad4b6a2f8b86201be684d4ab42
Nealll68 <hugoducry@gmail.com>
2020-06-10 15:47:13 +0200

    Rewrite code with native exec and fs.copyFile

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const argv = require('minimist')(process.argv.slice(2))
 
 const git = require('./lib/git')
 const npm = require('./lib/npm')
-//const app = require('./lib/app')
+const app = require('./lib/app')
 
 const run = async () => {
   const isUpdate = argv.update
@@ -58,42 +58,63 @@ const run = async () => {
     console.log(chalk.green('Packages installed'))
   } catch (e) {
     console.log(chalk.bgRed('An error happened while installing packages'))
-    //console.log(chalk.red(`Exit code : ${e.code}`))
     console.log(chalk.red(`Stderr : ${e}`))
     process.exit()
   }
-  process.exit()
 
   if (!isUpdate) {
     /*
       CREATE .ENV FILE
     */
 
-    await app.env()
-    console.log(chalk.green('.env file created'))
+    try {
+      await app.env()
+      console.log(chalk.green('.env file created'))
+    } catch (e) {
+      console.log(chalk.bgRed('An error happened while creating .env file'))
+      console.log(chalk.red(`Stderr : ${e}`))
+      process.exit()
+    }
 
     /*
       GENERATE APP KEY
     */
 
-    await app.key()
-    console.log(chalk.green('App key generated'))
+    try {
+      await app.key()
+      console.log(chalk.green('App key generated'))
+    } catch (e) {
+      console.log(chalk.bgRed('An error happened while generating app key'))
+      console.log(chalk.red(`Stderr : ${e}`))
+      process.exit()
+    }
   }
 
   /*
     RUN DATABASE MIGRATIONS
   */
 
-
-  await app.runMigrations()
-  console.log(chalk.green('Database migrations runned'))
+  try {
+    await app.runMigrations()
+    console.log(chalk.green('Database migrations runned'))
+  } catch (e) {
+    console.log(chalk.bgRed('An error happened while running database migrations'))
+    console.log(chalk.red(`Stderr : ${e}`))
+    process.exit()
+  }
 
   /*
     BUILD APP
   */
 
-  await app.build()
-  console.log(chalk.green('App builded'))
+  try {
+    await app.build()
+    console.log(chalk.green('App builded'))
+  } catch (e) {
+    console.log(chalk.bgRed('An error happened while building app'))
+    console.log(chalk.red(`Stderr : ${e}`))
+    process.exit()
+  }
 
   console.log(chalk.bgGreen('Styx has been successfully installed'))
 }
